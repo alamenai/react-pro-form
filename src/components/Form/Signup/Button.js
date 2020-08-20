702ea0a5217b5c63a420fb82378adfd7a807423a
MenaiAla <ed.sheeran.eng@gmail.com>
2020-08-20 15:59:02 -0700

    :cyclone: Move type and value inside class a static values

--- src/components/Form/Signup/Button.js
+++ src/components/Form/Signup/Button.js
@@ -2,16 +2,18 @@ import React from "react"
 import { ButtonBrand as SignUpButton } from "../../lib/Button"
 import { disabledContext } from "./index"
 
-const type = "submit"
-const value = "Sign up for Gitbank"
+
 
 class SignUp extends React.Component {
+
+    static type = "submit"
+    static value = "Sign up for Gitbank"
 
     render() {
         return (<disabledContext.Consumer >{
             ({ disabled }) => (<SignUpButton
-                type={type}
-                value={value}
+                type={SignUp.type}
+                value={SignUp.value}
                 disabled={disabled}
                 classes="font-semibold text-white mt-10" />)
         }
