7642a5f99e4e05d94e3e9281dbdfabb3dafede6b
MenaiAla <ed.sheeran.eng@gmail.com>
2020-08-21 04:07:27 -0700

    :cyclone: Remove ButtonDefault, renderButton and Add reponsive classes

--- src/components/lib/Button.jsx
+++ src/components/lib/Button.jsx
@@ -10,20 +10,16 @@ const Button = ({ type, value, event, classes, disabled }) => {
         onChange={event} />
 }
 
-const ButtonDefault = (props) => {
-    let defaultClasses = "w-full bg-gray-400 border outline-none h-12 font-helvetica text-base rounded-sm tracking-wider px-4 py-2 focus:outline-none"
-    return renderButton(props, defaultClasses)
-}
-
 const ButtonBrand = (props) => {
-    let defaultClasses = "w-full bg-brand border outline-none h-16 font-helvetica text-base rounded-lg tracking-wider px-4 py-2 focus:shadow-outline-sm cursor-pointer"
-    return renderButton(props, defaultClasses)
+    let extraSmallClasses = "xs:h-12 xs:mt-8 xs:text-sm "
+    let smallClasses = "sm:h-12 sm:mt-10 sm:text-sm "
+    let mediumClasses = "md:h-16 md:mt-12 md:text-base "
+    let defaultClasses = "w-full text-white bg-brand border outline-none h-16 font-helvetica text-base rounded-lg tracking-wider px-4 py-2 mt-10 focus:shadow-outline-sm cursor-pointer"
+    let allClasses = extraSmallClasses.concat(smallClasses).concat(mediumClasses).concat(defaultClasses)
+    return <Button {...props} classes={allClasses} />
 }
 
-const renderButton = (props, defaultClasses) => {
-    const { classes } = props
-    return <Button {...props} classes={defaultClasses + " " + classes} />
-}
+
 
 Button.propTypes = {
     type: PropTypes.string.isRequired,
@@ -33,4 +29,4 @@ Button.propTypes = {
 
 }
 
-export { ButtonDefault, ButtonBrand } 
+export { ButtonBrand } 
