a3bde61aa088be17411c85f1a5b30693c2745132
MenaiAla <ed.sheeran.eng@gmail.com>
2020-08-20 17:15:56 -0700

    :beetle: Add missed props

--- src/components/Form/withLabel.js
+++ src/components/Form/withLabel.js
@@ -3,12 +3,13 @@ import { TextDefault as Label } from "../lib/Text"
 
 const withLabel = Field => {
     class withLabel extends React.Component {
+
         render() {
             return <div className="mt-3">
                 <Label
                     content={Field.labelText}
                     classes="font-semibold text-sm" />
-                <Field />
+                <Field {...this.props} />
             </div>
         }
     }
