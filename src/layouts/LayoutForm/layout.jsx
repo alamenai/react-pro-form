ef1003b9eb6c769f66d0afbbad4f95cf5696e9fe
MenaiAla <ed.sheeran.eng@gmail.com>
2020-08-21 04:02:01 -0700

    :cyclone: Add responsive classes

--- src/layouts/LayoutForm/layout.jsx
+++ src/layouts/LayoutForm/layout.jsx
@@ -1,7 +1,10 @@
 import React from "react"
 
 const FormLayout = ({ children }) => (
-    <div className="rounded-xl px-12 py-10 w-1/3 bg-gray-100">
+    <div className="rounded-xl px-12 pt-4 pb-16 bg-gray-100 w-1/3
+                    xs:px-6 xs:w-full xs:mx-2 xs:mt-2
+                    sm:px-12 sm:w-full sm:mx-32 sm:mt-2
+                    md:px-12 md:w-full md:mx-48 md:mt-2">
         {children}
     </div>
 )
