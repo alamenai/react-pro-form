26b7a857e1731dc54dc17a4c735766e50e63e0aa
MenaiAla <ed.sheeran.eng@gmail.com>
2020-08-22 14:56:31 -0700

    :tada: Creae build setup

---
+++ build/precache-manifest.b70be6d8b68d76dec18ab979f6fb6edc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ed9aa858f2366c297c9e7ec71006898",
+    "url": "/react-pro-form/index.html"
+  },
+  {
+    "revision": "50759280589d4ac1555d",
+    "url": "/react-pro-form/static/css/main.29de0ad9.chunk.css"
+  },
+  {
+    "revision": "462cd3fb6d3039c238fd",
+    "url": "/react-pro-form/static/js/2.f158d119.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-pro-form/static/js/2.f158d119.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50759280589d4ac1555d",
+    "url": "/react-pro-form/static/js/main.065ed95f.chunk.js"
+  },
+  {
+    "revision": "a4dc6060ee04306b7c55",
+    "url": "/react-pro-form/static/js/runtime-main.894864bf.js"
+  }
+]);
